185975fe7af305dbb16488e5d1e742f4dec7970a
Brayzen <brian.ray.biz@gmail.com>
2015-09-03 21:23:59 -0700

    following rails cast

---
+++ README.md
@@ -0,0 +1,9 @@
+EPISODE #417.....
+
+I kept up until he pulled out a paste for the navigation bar that I wasn't going to type out.
+
+He was super quick...but it gave me the foundation of 'foundation' that I'll need for my site.
+
+YEEEEEHA!
+
+by Brian Ray
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'b9dee2f83abad41afc98623afe3878d099026d96d750d4af547f742a507513866cc4bb2d6db423997d9bc21f772ddeb2b681e928fa1103cb95e8168b0ffe95a1'
